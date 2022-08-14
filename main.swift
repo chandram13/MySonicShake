97b66f101c462e6a7f56f43a8f34030b8875991e
chandram13 <mschandr@uci.edu>
2022-08-14 13:55:13 -0700

    Added additonal factors, specifically the Sonic Blast order.

--- main.swift
+++ main.swift
@@ -7,11 +7,31 @@ import Foundation
 class uniqueShake {
 
     func combineShake {
+        var sizes = ["Mini","Small","Medium","Large"]
+        var mySize = []
+        var sonicBlast = ["Reese's Peanut Butter Cups","M&M's Minis Chocolate Candies","OREO Cookie Pieces","Snickers Bars","Butterfinger Pieces","Chocolate Chip Cookie Dough"]
         var flavors = ["Churro", "Strawberry Cheesecake", "Cheesecake", "OREO Cheesecake", "OREO Chocolate", "OREO & Reese's Peanut Butter", "Strawberry", "Chocolate", "Fresh Banana", "Vanilla", "Reese's Peanut Butter", "Hot Fudge", "Caramel"]
         var myShake = []
+        var wantmyShake = ""
+        var wantmyBlast = ""
+        if wantmyBlast == "yes":
+        while i = 0; myShake > i.length; i++{
+            sonicBlast[i] += myShake
+            break;
+        while i = 0; mySize > sizes.length; i++{
+                sizes[i] += mySize
+                break;
+            }
+        else print("Suggest to the user that they might want to order a different drink or a shake.")
+        }
+        if wantmyShake == "yes":
         while i = 0; myShake > i.length; i ++ {
             flavors[i] += myShake
             break;
+        while i = 0; mySize > sizes.length; i++{
+                sizes[i] += mySize
+                break;
+            }
         }
     }
 }
